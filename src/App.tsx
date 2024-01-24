f60a1096dcf47e41e1bb9ab0572d33953b56c5ed
streetlightkids <ltj_hq@hotmail.com>
2024-01-23 18:31:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import { useGetCountryList } from "./api";
 import CountryCard from "./CountryCard";
+import Header from "./Header";
+import SearchBar from "./SearchBar";
+import RegionDropdown from "./RegionDropdown";
+import "./App.scss";
 
 function App() {
-  const countries = useGetCountryList();
+  const [region, setRegion] = useState("");
+  const [name, setName] = useState("");
+  const countries = useGetCountryList({ region: region, name: name });
+  const isFiltering = name !== "" || region !== "";
+  const hasNoResults = countries.length === 0 && isFiltering;
+
   return (
     <div className="App">
-      {countries?.map((country) => {
-        return <CountryCard country={country} />;
-      })}
+      <Header />
+      <div className="search-filter-container">
+        <SearchBar name={name} setName={setName} />
+        <RegionDropdown region={region} setRegion={setRegion} />
+      </div>
+      {hasNoResults ? (
+        <p>
+          There were no matches for "{name}" and "{region}".
+        </p>
+      ) : (
+        <section className="countries-container">
+          {countries?.map((country) => {
+            return <CountryCard country={country} />;
+          })}
+        </section>
+      )}
     </div>
   );
 }
