d67ff415b50e478f30c95922ded17b80542186e3
streetlightkids <ltj_hq@hotmail.com>
2024-01-30 19:01:42 -0700

    Imports react-router-dom and restructures file

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,20 @@
-import { useState } from "react";
-import { useGetCountryList } from "./api";
-import CountryCard from "./CountryCard";
 import Header from "./Header";
-import SearchBar from "./SearchBar";
-import RegionDropdown from "./RegionDropdown";
 import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import CountryDetailPage from "./CountryDetailPage";
 
 function App() {
-  const [region, setRegion] = useState("");
-  const [name, setName] = useState("");
-  const countries = useGetCountryList({ region: region, name: name });
-  const isFiltering = name !== "" || region !== "";
-  const hasNoResults = countries.length === 0 && isFiltering;
-
   return (
-    <div className="App">
-      <Header />
-      <div className="search-filter-container">
-        <SearchBar name={name} setName={setName} />
-        <RegionDropdown region={region} setRegion={setRegion} />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:alpha3Code" element={<CountryDetailPage />} />
+        </Routes>
       </div>
-      {hasNoResults ? (
-        <p>
-          There were no matches for "{name}" and "{region}".
-        </p>
-      ) : (
-        <section className="countries-container">
-          {countries?.map((country) => {
-            return <CountryCard country={country} />;
-          })}
-        </section>
-      )}
-    </div>
+    </BrowserRouter>
   );
 }
 
