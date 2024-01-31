6a964d40c707349e9580923062a7a9fbc6935d21
streetlightkids <ltj_hq@hotmail.com>
2024-01-30 19:00:02 -0700

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,13 @@
+import "./Header.scss";
+/**
+ * @NOTE
+ * 🚨 need new icon for button and styling (no cursor on hover)
+ * 🚨 need dark mode functional
+ */
 export default function Header() {
   return (
     <header className="Header">
-      <p className="site-title">Where in the world?</p>{" "}
+      <p className="site-title">Where in the world?</p>
       <button className="ui-toggle">☾ Dark Mode</button>
     </header>
   );
